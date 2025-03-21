03a0c4fcf87e1a63f9efc2c6ca7d1f870fe4854c
Matthew Robillard <robillard.matthew22@gmail.com>
2025-03-21 14:57:36 -0700

    Should be fire now

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -31,7 +31,7 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/fullstack-decal/website/edit/main",
+          editUrl: "https://github.com/MJRobillard/V2_SalsaBoard/tree/main/",
         },
         theme: {
           customCss: "./src/css/custom.css",
